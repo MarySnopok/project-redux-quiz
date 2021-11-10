5a85f5981a9607f78686f4f8c29f9a13acc757ed
e-delsol <80672920+e-delsol@users.noreply.github.com>
2021-11-10 18:02:31 +0100

    summery of correct answers

--- src/components/smallComponents/Summary.js
+++ src/components/smallComponents/Summary.js
@@ -1,21 +1,35 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import { quiz } from "../../reducers/quiz";
 
 export const Summary = () => {
-  const dispatch = useDispatch();
+	const questions = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex]);
 
-  const anotherTry = () => {
-    dispatch(quiz.actions.restart());
-  };
+	const answers = useSelector((state) => state.quiz.answers);
 
-  return (
-    <>
-      <div>Thats the draft of summary page</div>
-      <button className="submit-button" type="submit" onClick={anotherTry}>
-        Try Again
-      </button>
-    </>
-  );
+	const dispatch = useDispatch();
+
+	const anotherTry = () => {
+		dispatch(quiz.actions.restart());
+	};
+	console.log("this is answer", answers);
+
+	return (
+		<div>
+			<p>Thats the draft of summary page</p>
+			<button className="submit-button" type="submit" onClick={anotherTry}>
+				Try Again
+			</button>
+			{answers.map((answer) => (
+				<div key={answer.questionId}>
+					<h2>
+						Question {answer.questionId} is {String(answer.isCorrect)}
+					</h2>
+				</div>
+			))}
+		</div>
+	);
 };
+
+//<h2>{answer.isCorrect.filter(true).length}/5 correct answers</h2>
